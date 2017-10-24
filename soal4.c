5483752518e61ae3805a0d0e8cd9460b5db99494
Muhammad Ivan Riansyah Putra <33018078+shakellzero@users.noreply.github.com>
2017-10-24 22:20:09 +0700

    Add files via upload
    tambah fungsi thread

--- soal4.c
+++ soal4.c
@@ -6,7 +6,7 @@ int total=1;
 
 main()
 {
-	int number, count;
+	int number;
 	pthread_t t;
 	printf("Masukkan angka yang akan difaktorialkan : ");
 	scanf("%d", &number);
@@ -15,4 +15,16 @@ main()
 	pthread_join(t,NULL);
 
 	printf("Hasil dari faktorial adalah  %d \n", total);
+}
+
+void *faktorial(void *no)
+{
+    int *nomor,count;
+    nomor=(int*)no;
+
+    for(count=1;count<=*nomor;count=count+1)
+    {
+        total = total*count;
+    }
+    pthread_exit(NULL);
 }
